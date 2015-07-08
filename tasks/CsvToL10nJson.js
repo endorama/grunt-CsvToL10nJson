1e6cacfe7344c778b07942a66692b4b310e4d95a
Edoardo Tenani <edoardo.tenani@gmail.com>
2015-07-08 16:20:19 +0200

    fix destination path when using expand: true

--- tasks/CsvToL10nJson.js
+++ tasks/CsvToL10nJson.js
@@ -10,6 +10,7 @@
 
 var csvToL10nJson = require('CsvToL10nJson');
 var fse = require('fs-extra');
+var path = require('path');
 
 module.exports = function(grunt) {
 
@@ -24,6 +25,16 @@ module.exports = function(grunt) {
     var done = this.async();
 
     this.files.forEach(function (f) {
+
+      // if destination path ends with .csv
+      if (f.dest.indexOf('.csv') === f.dest.length-4) {
+        // we calculate the correct path
+        f.dest = path.dirname(f.dest);
+        // if expand: false, we warn user about misconfiguration
+        if (!f.orig.expand) {
+          grunt.fail.warn('You destination path is invalid, should not end with .csv');
+        }
+      }
 
       fse.ensureDirSync(f.dest);
 
